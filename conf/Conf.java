5da55208efd0fb16c520f75453691307f4a1e43b
Heo-SoYeong <131340824+hsy0125@users.noreply.github.com>
2024-12-22 01:22:51 +0900

    Update Conf.java

--- conf/Conf.java
+++ conf/Conf.java
@@ -2,6 +2,6 @@ package io.mobile.finalproject.conf;
 
 public interface Conf {
     String DB_URL = "jdbc:postgresql://dbclass.wisoft.io:10008/hyun";
-    String DB_USER = "hyun";
-    String DB_PASSWORD = "gusrud03071@";
+    String DB_USER = "사용자ID";
+    String DB_PASSWORD = "비밀번호";
 }
